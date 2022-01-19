59bdfe8cf98649c0ca10ceb595bd6a32305ac460
Mert Selcuk Demir <mertselcukdemir@gmail.com>
2022-01-20 00:22:16 +0300

    Create RepoListFragment as start destination of navigation

---
+++ feature/repo/src/main/java/com/mertselcukdemir/repo/ui/list/RepoListFragment.kt
@@ -0,0 +1,11 @@
+package com.mertselcukdemir.repo.ui.list
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by mertselcukdemir on 20.01.2022
+ * All rights reserved.
+ */
+
+class RepoListFragment : Fragment() {
+}
