f03bd5f45e0aca3cdb45514886b595f58324f7c5
Mert Selcuk Demir <mertselcukdemir@gmail.com>
2022-01-19 16:02:38 +0300

    Create buildSrc directory for Gradle Dependency Management

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,29 @@
+/**
+ * Created by mertselcukdemir on 19.01.2022
+ * All rights reserved.
+ */
+
+plugins {
+    `java-gradle-plugin`
+    `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://plugins.gradle.org/m2/")
+}
+
+object PluginsVersions {
+    const val GRADLE_ANDROID = "7.0.4"
+    const val GRADLE_VERSIONS = "0.41.0"
+    const val KOTLIN = "1.6.10"
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:${PluginsVersions.GRADLE_ANDROID}")
+    implementation("com.github.ben-manes:gradle-versions-plugin:${PluginsVersions.GRADLE_VERSIONS}")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${PluginsVersions.KOTLIN}")
+}
