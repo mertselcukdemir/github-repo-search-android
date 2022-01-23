ba4d4c0e3f086c6160923cf0af6f75cd4f267c49
mertselcukdemir <mertselcukdemir@gmail.com>
2022-01-23 23:14:17 +0300

    Create Base ListAdapter, ViewHolder & ViewState

---
+++ commons/ui/src/main/kotlin/com/mertselcukdemir/ui/base/BaseViewState.kt
@@ -0,0 +1,11 @@
+package com.mertselcukdemir.ui.base
+
+/**
+ * Created by mertselcukdemir on 21.01.2022
+ * All rights reserved.
+ */
+
+/**
+ * Base state interface to describe different state of the view.
+ */
+interface BaseViewState
