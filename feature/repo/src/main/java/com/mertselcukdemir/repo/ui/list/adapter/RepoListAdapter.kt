26dbc5234e23a0f6fe077012d075a53db80843a0
mertselcukdemir <mertselcukdemir@gmail.com>
2022-01-23 23:08:41 +0300

    Create adapter package to display repository list

---
+++ feature/repo/src/main/java/com/mertselcukdemir/repo/ui/list/adapter/RepoListAdapter.kt
@@ -0,0 +1,9 @@
+package com.mertselcukdemir.repo.ui.list.adapter
+
+/**
+ * Created by mertselcukdemir on 21.01.2022
+ * All rights reserved.
+ */
+
+class RepoListAdapter {
+}
---
+++ feature/repo/src/main/java/com/mertselcukdemir/repo/ui/list/adapter/holders/RepoListViewHolder.kt
@@ -0,0 +1,33 @@
+package com.mertselcukdemir.repo.ui.list.adapter.holders
+
+import android.view.LayoutInflater
+import com.mertselcukdemir.core.data.RepositoryModel
+import com.mertselcukdemir.repo.databinding.AdapterRepoBinding
+import com.mertselcukdemir.ui.base.BaseViewHolder
+import com.mertselcukdemir.repo.ui.list.RepoListFragment
+
+/**
+ * Created by mertselcukdemir on 21.01.2022
+ * All rights reserved.
+ */
+
+class RepoListViewHolder(
+    inflater: LayoutInflater
+) : BaseViewHolder<AdapterRepoBinding>(
+    binding = AdapterRepoBinding.inflate(inflater)
+) {
+
+    /**
+     * Bind view data binding variables.
+     *
+     * @param model Repo list item.
+     * @param callBack callback that returns [RepositoryModel] to [RepoListFragment] for navigating to next fragment.
+     */
+    fun bind(model: RepositoryModel, callBack: (repo: RepositoryModel) -> Unit) {
+        binding.model = model
+        binding.root.setOnClickListener {
+            callBack.invoke(model)
+        }
+        binding.executePendingBindings()
+    }
+}
